c42538e35636596ebdbc03ff4e1fbe18b48be9f0
Guillaume R.-Mathieu <guillaume.rochefort.mathieu@gmail.com>
2019-03-21 20:29:04 -0400

    Includes the parsing of a Mesh. Needs to do the intersection

--- Mesh.cpp
+++ Mesh.cpp
@@ -19,6 +19,42 @@ bool Mesh::intersect(Ray& ray, float& t)
 
 void Mesh::parse(std::istream& input)
 {
+	std::string line;
+	char colon = ':';
+
+	std::istringstream lineStream;
+
+	// Read object file
+	std::getline(input, line);
+	std::string stringPath = "./scenes/" + trim(line.substr(line.find(colon) + 1, line.length()));
+	loadOBJ(stringPath.c_str(), this->indices, this->vertices, this->normals, this->UVs);
+
+	Color color;
+	Material material;
+
+	// Parse ambient color
+	std::getline(input, line);
+	std::string stringAmbientColor = trim(line.substr(line.find(colon) + 1, line.length()));
+	color.setAmbientColor(readVec3(std::istringstream(stringAmbientColor)));
+
+	// Parse diffuse color
+	std::getline(input, line);
+	std::string stringDiffuseColor = trim(line.substr(line.find(colon) + 1, line.length()));
+	color.setDiffuseColor(readVec3(std::istringstream(stringDiffuseColor)));
+
+	// Parse specular color
+	std::getline(input, line);
+	std::string stringSpecularColor = trim(line.substr(line.find(colon) + 1, line.length()));
+	color.setSpecularColor(readVec3(std::istringstream(stringSpecularColor)));
+
+	material.setColor(color);
+
+	// Prase shininess
+	std::getline(input, line);
+	std::string stringShininess = trim(line.substr(line.find(colon) + 1, line.length()));
+	material.setShininess(std::stof(stringShininess));
+
+	this->setMaterial(material);
 }
 
 glm::vec3 Mesh::calculateNormal(glm::vec3 position)
--- Mesh.h
+++ Mesh.h
@@ -1,11 +1,16 @@
 #ifndef MESH_H
 #define MESH_H
-#include "PhysicalObject.h"
-#include "Material.h"
-#include "Ray.h"
+#include <string>
+#include <sstream>
+#include <vector>
 #include "./glm/glm.hpp"
 #include "./glm/gtc/matrix_transform.hpp"
 #include "./glm/gtc/type_ptr.hpp"
+#include "PhysicalObject.h"
+#include "OBJLoader.h"
+#include "Material.h"
+#include "Ray.h"
+#include "Utils.h"
 
 class Mesh: public PhysicalObject {
 public:
@@ -16,5 +21,9 @@ public:
 	glm::vec3 calculateNormal(glm::vec3 position);
 protected:
 	const static Material DEFAULT_MATERIAL;
+	std::vector<glm::vec3> normals;
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec2> UVs;
+	std::vector<int> indices;
 };
 #endif
